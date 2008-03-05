4235fbfc1620dbf143fda080ccec6465bf80d751
Rob Cameron <cannikinn@gmail.com>
2008-03-05 12:04:14 -0800

    Got breadcrumbs working

--- app/controllers/classifieds_controller.rb
+++ app/controllers/classifieds_controller.rb
@@ -2,23 +2,23 @@ class ClassifiedsController < ApplicationController
 
   def index
     if params[:subcategory]
-      # find all ads for this subcategory
-      category = Category.find_by_sql(  "select child.* " +
-                                        "from   categories child inner join categories parent on child.parent_id = parent.id " +
-                                        "where  child.url = '#{params[:subcategory]}' and parent.url = '#{params[:category]}'" +
-                                        "order by child.created_at asc")
-      @ads = Ad.find_all_by_category_id(category, :order => 'created_at desc')
+      @category = Category.find_by_full_url(params)
+      @ads = @category.ads
+      @breadcrumbs = [ { :name => @category.parent.name, :url => url_for(:category => @category.parent.url, :subcategory => nil) },
+                       { :name => @category.name, :url => url_for(:category => @category.parent.url, :subcategory => @category.url) } ]
     elsif params[:category]
       # find all ads by the children of this category
-      category = Category.find_by_url(params[:category], :conditions => 'parent_id is null')
-      categories = category.children.collect do |cat|
+      @category = Category.find_by_url(params[:category], :conditions => 'parent_id is null')
+      category_ids = @category.children.collect do |cat|
         cat.id
       end
-      categories << category.id
-      @ads = Ad.find_all_by_category_id(categories, :order => 'created_at desc')
+      # append the parent category itself
+      category_ids << @category.id
+      @ads = Ad.find_all_by_category_id(category_ids, :order => 'created_at desc')
+      @breadcrumbs = [ { :name => @category.name, :url => url_for(:category => @category.url, :subcategory => nil) } ]
     else
       # just a list of all categories
-      @categories = Category.find_roots
+      @categories = Category.find_all_roots
     end
   end
   
--- app/models/category.rb
+++ app/models/category.rb
@@ -4,8 +4,18 @@ class Category < ActiveRecord::Base
   has_many :ads
   
   # only finds the root categories (those with no parent)
-  def self.find_roots
+  def self.find_all_roots
     find(:all, :conditions => 'parent_id is null')
+  end
+
+  # find a specific category by the full URL
+  def self.find_by_full_url(params)
+    category = Category.find_by_sql(  "select   child.id " +
+                                      "from     categories child inner join categories parent on child.parent_id = parent.id " +
+                                      "where    child.url = '#{params[:subcategory]}' and parent.url = '#{params[:category]}'" +
+                                      "order by child.created_at asc")
+    # do a regular ActiveRecord find so that we have a normal ActiveRecord object
+    Category.find(category).first
   end
   
   # set the url for this category automatically before it's saved
