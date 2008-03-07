7303924e63f7e8ae0a5711b73e8179282b139b14
Rob Cameron <cannikinn@gmail.com>
2008-03-07 08:34:00 -0800

    has_at_least? hash extension works properly

--- config/initializers/extend_hash.rb
+++ config/initializers/extend_hash.rb
@@ -1,7 +1,7 @@
 class Hash
   def has_at_least?(other)
     other.keys.all? do |key|
-      (self[key] && self[key] == other[key]) || other[key] == nil
+      (self[key] && self[key] == other[key]) || (other[key] == nil && !self.has_key?(key))
     end
   end
 end
